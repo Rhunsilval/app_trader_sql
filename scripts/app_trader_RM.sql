229055d5f829736fde96f07f4bd803b456d1e163
Ryan-Maurer <RMaurer23@gmail.com>
2021-08-19 21:23:22 -0500

    app_trader_2nd

--- scripts/app_trader_RM.sql
+++ scripts/app_trader_RM.sql
@@ -1,9 +1,25 @@
 --reference 
+SELECT *
+FROM play_store_apps
+LIMIT 10;
+
 SELECT DISTINCT name,category,review_count,rating
 FROM play_store_apps
 WHERE type = 'Free'
 	AND review_count <1000000
 ORDER BY rating ;
 
-SELECT name,
-	CASE WHEN rating BETWEEN 0 AND 
+SELECT
+	name,
+	price,
+	CAST((CASE WHEN price <= 1.00 THEN 10000
+	ELSE (price*10000) END) AS money) AS purchase_cost,
+	review_count,
+	rating,
+	ROUND((rating/.5), 1) AS longevity,
+	content_rating,
+	primary_genre AS genres
+FROM app_store_apps AS iphone
+
+
+
