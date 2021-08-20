e8e5db06e1a9928426bb748342e7f3f036b5cbf2
eporourke <86206720+eporourke@users.noreply.github.com>
2021-08-19 21:22:31 -0500

    08/19/2021

--- scripts/eorapp.sql
+++ scripts/eorapp.sql
@@ -1,8 +1,28 @@
-SELECT
+SELECT 
 	name
-FROM app_store_apps
-UNION ALL
-SELECT
+	FROM app_store_apps
+UNION
+	SELECT
 	name
-FROM play_store_apps
-ORDER BY name;
+	FROM play_store_apps
+ORDER BY name DESC;
+
+SELECT
+	name,
+	price,
+	CAST((CASE WHEN price <= 1.00 THEN 10000
+	ELSE (price*10000) END) AS money) AS purchase_cost,
+	review_count,
+	rating,
+	ROUND((rating/.5), 1) AS longevity,
+	content_rating,
+	primary_genre AS genres
+FROM app_store_apps AS iphone
+
+SELECT*
+FROM
+app_store_apps
+INNER JOIN play_store_apps
+ON app_store_apps.name=play_store_apps.name
+
+
